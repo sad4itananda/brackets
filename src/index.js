5c2a8d19e839a9fb81e373ca7586e122b9de3364
V.Vlasiuk <4ipej6ij@jmail.com>
2019-03-24 20:24:49 +0300

    brackets solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+	let arrFromStr = str.split('');
+	let resultBoolean;
+
+	const checkOverlap = (arr, config) => {
+		if (arr == 0) return true;
+		for (let i = 0; i < arr.length - 1; i++) {
+			for (let j = 0; j < config.length; j++) {
+				if	((arr[i] === config[j][0]) && (arr[i + 1] === config[j][1])) {
+					arr.splice(i, 2);
+					return;
+				} 
+			}	
+		} return false;
+	};
+	
+	while (resultBoolean == undefined) {
+		resultBoolean = checkOverlap(arrFromStr, bracketsConfig);
+	};
+	return resultBoolean;
+};
